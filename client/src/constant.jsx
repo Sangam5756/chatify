63d572061078eccb2ced247e3ef63800c1f742f1
sangam5756 <sangammunde3@gmail.com>
2025-01-13 07:37:33 +0530

    url changed

--- client/src/constant.jsx
+++ client/src/constant.jsx
@@ -1,4 +1,4 @@
-export const ProductionbackendUrl = "https://web-backend-5pp9.onrender.com"
+export const ProductionbackendUrl = "https://chatify-one-chi.vercel.app"
 // export const ProductionbackendUrl = "http://localhost:4000"
 
-export const  LocalbackendUrl= "http://localhost:4000"
+// export const  LocalbackendUrl= "http://localhost:4000"
