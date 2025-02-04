1de0454a6adf913049af5bc2dd44a992d34bafc8
sangam5756 <sangammunde3@gmail.com>
2025-02-04 08:16:56 +0530

    done

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -8,24 +8,6 @@ import Loading from "./Loading";
 const Body = () => {
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const siteUp = async () => {
-      setLoading(true);
-      try {
-        const res = await fetch(ProductionbackendUrl, {
-          method: "GET",
-        });
-
-        const response = await res.json();
-        if (response?.up) {
-          setLoading(false);
-        }
-      } catch (error) {
-        setLoading(false);
-      }
-    };
-    siteUp();
-  }, []);
 
   return (
     <div>
