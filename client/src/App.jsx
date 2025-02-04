db3e145f17faa011f56ead53e14031d909661330
sangam5756 <sangammunde3@gmail.com>
2025-02-04 07:52:02 +0530

    backend url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Room from "./components/Room";
 import Body from "./components/Body";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 
+setInterval(() => {
+  fetch("https://ai-coder-cogm.onrender.com/get-tasks")
+    .then((res) => console.log("called"))
+    .catch((err) => console.log(err));
+},600000);
 
 const App = () => {
   return (
     <div>
-              <ToastContainer position="top-center" />
+      <ToastContainer position="top-center" />
 
       <BrowserRouter>
         <Routes>
--- client/src/constant.jsx
+++ client/src/constant.jsx
@@ -1,4 +1,5 @@
-export const ProductionbackendUrl = "https://web-backend-5pp9.onrender.com"
+// export const ProductionbackendUrl = "https://web-backend-5pp9.onrender.com"
+export const ProductionbackendUrl = "https://kind-corliss-sangammundhe-982b9296.koyeb.app"
 // export const ProductionbackendUrl = "http://localhost:4000"
 
-// export const  LocalbackendUrl= "http://localhost:4000"
+export const  LocalbackendUrl= "http://localhost:4000"
